07bf7f5e2dd14739900ce2a5eaebe6f7be34a328
tcnikita <carkovnikita@gmail.com>
2023-02-06 19:15:02 +0300

    bs-1 trying to learn git branches

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -49,7 +49,7 @@ class PostController extends Controller
 
     public function update()
     {
-        $post = Post::find(6);
+        $post = Post::find(4);
 
         $post->update([
             'title' => 'updated',
